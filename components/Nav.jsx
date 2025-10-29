d78bb18b2c7679f20dc20680125df6eba31491df
Yevhenii Liashenko <opanabl9@gmail.com>
2025-10-29 21:08:04 +0100

    make active links in Nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,20 +1,46 @@
 'use client'
 
-import {IoHome} from 'react-icons/io5'
-import {IoIosSearch} from 'react-icons/io'
-import {GoBell} from 'react-icons/go'
-import {MdOutlineMail} from 'react-icons/md'
-import {FaRegUser} from 'react-icons/fa'
+import {GoBell, GoBellFill, GoHome, GoHomeFill} from 'react-icons/go'
+
+import {FaRegUser, FaUser} from 'react-icons/fa6'
 import Link from 'next/link'
 import {usePathname} from 'next/navigation'
+import {CiSearch} from 'react-icons/ci'
+import {IoSearch} from 'react-icons/io5'
+import {TbMail, TbMailFilled} from 'react-icons/tb'
 
 
 function Nav() {
-  const navlinks = [{icon: <IoHome />, name: 'home', href: '/'},
-    {icon: <IoIosSearch />, name: 'explore', href: '/explore'},
-    {icon: <GoBell />, name: 'notifications', href: '/notifications'},
-    {icon: <MdOutlineMail />, name: 'messages', href: '/messages'},
-    {icon: <FaRegUser />, name: 'profile', href: '/profile'}]
+  const navlinks = [{
+    icon: <GoHome />,
+    activeIcon: <GoHomeFill />,
+    name: 'home',
+    href: '/'
+  },
+    {
+      icon: <CiSearch />,
+      activeIcon: <IoSearch />,
+      name: 'explore',
+      href: '/explore'
+    },
+    {
+      icon: <GoBell />,
+      activeIcon: <GoBellFill />,
+      name: 'notifications',
+      href: '/notifications'
+    },
+    {
+      icon: <TbMail />,
+      activeIcon: <TbMailFilled />,
+      name: 'messages',
+      href: '/messages'
+    },
+    {
+      icon: <FaRegUser />,
+      activeIcon: <FaUser />,
+      name: 'profile',
+      href: '/profile'
+    }]
 
   const pathname = usePathname()
 
@@ -27,7 +53,7 @@ function Nav() {
           href={link.href}
           className={`flex items-center gap-2 ${pathname === link.href ? 'font-bold text-white' : 'text-gray-300'}`}
         >
-          <span className="text-2xl ">{link.icon}</span>
+          <span className="text-2xl ">{pathname === link.href ? link.activeIcon : link.icon}</span>
           <p className="text-lg capitalize">{link.name}</p>
         </Link>
       </li>
